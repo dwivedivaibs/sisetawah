9d099fac205acc2192814a50afa7c540550468f5
Vaibhav D <vaibhav111dwivedi@gmail.com>
2022-07-12 17:23:39 +0530

    fixed typo

--- db/migrate/20220629104813_add_entry.rb
+++ db/migrate/20220629104813_add_entry.rb
@@ -1,5 +1,5 @@
 class AddEntry < ActiveRecord::Migration[6.1]
   def change
-    AdminUser.create!(email: 'admin@sisetawah.com', password: 'Admin@sisetwah', password_confirmation: 'Admin@sisetwah')
+    User.create!(email: 'admin@sisetawah.com', password: 'Admin@sisetwah', password_confirmation: 'Admin@sisetwah')
   end
 end
