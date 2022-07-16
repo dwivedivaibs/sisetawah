8c93e78f6192afbd93282a194a439440a22999eb
Vaibhav D <vaibhav111dwivedi@gmail.com>
2022-07-16 10:37:26 +0530

    Added entries

--- db/migrate/20220629104813_add_entry.rb
+++
@@ -1,5 +0,0 @@
-class AddEntry < ActiveRecord::Migration[6.1]
-  def change
-    User.create!(email: 'admin@sisetawah.com', password: 'Admin@sisetwah', password_confirmation: 'Admin@sisetwah')
-  end
-end
